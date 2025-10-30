0288aeef928d750c8178250421e317931b76238f
Krasimir Stefanov <kraskoo87@gmail.com>
2025-10-30 21:48:25 +0200

    Fix logic and add test-data

--- src/Employees.Application/Services/PeriodService.cs
+++ src/Employees.Application/Services/PeriodService.cs
@@ -7,7 +7,8 @@ public class PeriodService : IPeriodService
 {
     public ResultPair GetLongestTimePeriod(IEnumerable<ProjectBindingModel> projects)
     {
-        SortedSet<ResultPair> commonProjects = new(new ResultPairTotalDaysComparer());
+        SortedDictionary<int, SortedSet<ResultPair>> commonProjects = [];
+        SortedDictionary<int, int> workedDaysTogether = [];
         var bestResult = new ResultPair { TotalDays = 0 };
         var employeeProjects = projects.GroupBy(p => p.ProjectID)
             .ToDictionary(g => g.Key, g => g.OrderBy(x => x.EmpID).ToList());
@@ -23,32 +24,44 @@ public class PeriodService : IPeriodService
                     var workedTogether = GetOverlapDays(
                         firstEmployee.DateFrom, firstEmployee.DateTo,
                         secondEmployee.DateFrom, secondEmployee.DateTo);
-                    if (workedTogether > 0)
+                    if (workedTogether <= 0)
                     {
-                        commonProjects.Add(new()
-                        {
-                            FirstEmployeeId = firstEmployee.EmpID,
-                            SecondEmployeeId = secondEmployee.EmpID,
-                            ProjectId = employeeProject.Key,
-                            TotalDays = workedTogether
-                        });
+                        continue;
                     }
+
+                    var key = firstEmployee.EmpID + secondEmployee.EmpID;
+                    if (!commonProjects.ContainsKey(key))
+                    {
+                        commonProjects[key] = new SortedSet<ResultPair>(new ResultPairTotalDaysComparer());
+                        workedDaysTogether[key] = 0;
+                    }
+
+                    commonProjects[key].Add(new()
+                    {
+                        FirstEmployeeId = firstEmployee.EmpID,
+                        SecondEmployeeId = secondEmployee.EmpID,
+                        ProjectId = employeeProject.Key,
+                        TotalDays = workedTogether
+                    });
+                    workedDaysTogether[key] += workedTogether;
                 }
             }
         }
 
-        var winnerPair = commonProjects.FirstOrDefault();
-        if (winnerPair is null)
+        var keys = workedDaysTogether.OrderByDescending(x => x.Value);
+        if (!keys.Any())
         {
             return new();
         }
 
-        bestResult.CommonProjects = commonProjects.Where(x =>
-            x.FirstEmployeeId == winnerPair.FirstEmployeeId &&
-            x.SecondEmployeeId == winnerPair.SecondEmployeeId);
+        var winnerKey = keys.FirstOrDefault().Key;
+        var winnerPair = commonProjects[winnerKey];
+        var firstProject = winnerPair.FirstOrDefault()!;
 
-        bestResult.FirstEmployeeId = winnerPair.FirstEmployeeId;
-        bestResult.SecondEmployeeId = winnerPair.SecondEmployeeId;
+        bestResult.CommonProjects = winnerPair;
+
+        bestResult.FirstEmployeeId = firstProject.FirstEmployeeId;
+        bestResult.SecondEmployeeId = firstProject.SecondEmployeeId;
         bestResult.TotalDays = bestResult.CommonProjects.Sum(x => x.TotalDays);
         return bestResult;
     }
